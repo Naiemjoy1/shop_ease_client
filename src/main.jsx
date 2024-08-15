d178ec12e9c198f5957cbdd3c9e67d55b0cc7631
Naiem Hasan <naiemjoy1@gmail.com>
2024-08-15 10:07:51 +0600

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import { StrictMode } from "react";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
