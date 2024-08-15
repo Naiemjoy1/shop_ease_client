552d268111db4192d5f227f6ba8e8af6f9ba6d2d
Naiem Hasan <naiemjoy1@gmail.com>
2024-08-15 21:45:39 +0600

    product pagination

--- src/Pages/Home/Products/Products.jsx
+++ src/Pages/Home/Products/Products.jsx
@@ -3,6 +3,7 @@ import ProductsCard from "./ProductsCard";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [totalProducts, setTotalProducts] = useState(0);
   const [sortCriteria, setSortCriteria] = useState("default");
   const [brand, setBrand] = useState("all");
   const [category, setCategory] = useState("all");
@@ -10,20 +11,28 @@ const Products = () => {
   const [brands, setBrands] = useState([]);
   const [categories, setCategories] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
-    fetch("http://localhost:3000/products")
+    fetch(
+      `http://localhost:3000/products?page=${currentPage}&size=${itemsPerPage}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data);
-        const uniqueBrands = [...new Set(data.map((product) => product.brand))];
+        setProducts(data.products);
+        setTotalProducts(data.totalProducts);
+
+        const uniqueBrands = [
+          ...new Set(data.products.map((product) => product.brand)),
+        ];
         const uniqueCategories = [
-          ...new Set(data.map((product) => product.category)),
+          ...new Set(data.products.map((product) => product.category)),
         ];
         setBrands(uniqueBrands);
         setCategories(uniqueCategories);
       });
-  }, []);
+  }, [currentPage, itemsPerPage]);
 
   const handleSortChange = (e) => {
     setSortCriteria(e.target.value);
@@ -69,72 +78,129 @@ const Products = () => {
       }
     });
 
+  const numberOfPages = Math.ceil(totalProducts / itemsPerPage);
+  const pages = Array.from({ length: numberOfPages }, (_, index) => index);
+
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < numberOfPages - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handleItemsPerPage = (e) => {
+    setItemsPerPage(parseInt(e.target.value));
+    setCurrentPage(0);
+  };
+
   return (
-    <div className="flex justify-between gap-8">
-      <div className="mb-4 flex flex-col gap-4 w-1/4">
-        <input
-          type="text"
-          placeholder="Search for products"
-          value={searchQuery}
-          onChange={handleSearchChange}
-          className="p-2 border rounded w-full"
-        />
-
-        <div className=" space-y-4">
-          <select
-            value={sortCriteria}
-            onChange={handleSortChange}
+    <div>
+      <div className="flex justify-between gap-8">
+        <div className="mb-4 flex flex-col gap-4 w-1/4">
+          <input
+            type="text"
+            placeholder="Search for products"
+            value={searchQuery}
+            onChange={handleSearchChange}
             className="p-2 border rounded w-full"
-          >
-            <option value="default">Sort By</option>
-            <option value="price-low-high">Price: Low to High</option>
-            <option value="price-high-low">Price: High to Low</option>
-            <option value="date-newest">Date Added: Newest First</option>
-          </select>
+          />
 
-          <select
-            value={brand}
-            onChange={handleBrandChange}
-            className="p-2 border rounded w-full"
-          >
-            <option value="all">All Brands</option>
-            {brands.map((brand) => (
-              <option key={brand} value={brand}>
-                {brand}
-              </option>
-            ))}
-          </select>
+          <div className="space-y-4">
+            <select
+              value={sortCriteria}
+              onChange={handleSortChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="default">Sort By</option>
+              <option value="price-low-high">Price: Low to High</option>
+              <option value="price-high-low">Price: High to Low</option>
+              <option value="date-newest">Date Added: Newest First</option>
+            </select>
 
-          <select
-            value={category}
-            onChange={handleCategoryChange}
-            className="p-2 border rounded w-full"
-          >
-            <option value="all">All Categories</option>
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
+            <select
+              value={brand}
+              onChange={handleBrandChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="all">All Brands</option>
+              {brands.map((brand) => (
+                <option key={brand} value={brand}>
+                  {brand}
+                </option>
+              ))}
+            </select>
 
-          <select
-            value={priceRange}
-            onChange={handlePriceRangeChange}
-            className="p-2 border rounded w-full"
-          >
-            <option value="all">All Price Ranges</option>
-            <option value="low">Low (0 - 50)</option>
-            <option value="mid">Mid (50 - 100)</option>
-            <option value="high">High (100+)</option>
-          </select>
+            <select
+              value={category}
+              onChange={handleCategoryChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="all">All Categories</option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+
+            <select
+              value={priceRange}
+              onChange={handlePriceRangeChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="all">All Price Ranges</option>
+              <option value="low">Low (0 - 50)</option>
+              <option value="mid">Mid (50 - 100)</option>
+              <option value="high">High (100+)</option>
+            </select>
+          </div>
         </div>
-      </div>
 
-      <div className="grid grid-cols-3 justify-between gap-5 w-9/12">
-        {filteredProducts.map((product) => (
-          <ProductsCard key={product._id} product={product} />
-        ))}
+        <div className="w-9/12">
+          <div className="grid grid-cols-3 gap-5">
+            {filteredProducts.map((product) => (
+              <ProductsCard key={product._id} product={product} />
+            ))}
+          </div>
+
+          {/* Pagination */}
+          <div className="mt-8 flex justify-evenly items-center">
+            <button onClick={handlePrevPage} className="btn btn-primary">
+              Previous
+            </button>
+            <div>
+              {pages.map((page) => (
+                <button
+                  onClick={() => setCurrentPage(page)}
+                  key={page}
+                  className={`btn ${
+                    page === currentPage ? "btn-active btn-primary" : ""
+                  }`}
+                >
+                  {page + 1}
+                </button>
+              ))}
+            </div>
+            <button onClick={handleNextPage} className="btn btn-primary">
+              Next
+            </button>
+            <select
+              value={itemsPerPage}
+              onChange={handleItemsPerPage}
+              className="ml-4"
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
