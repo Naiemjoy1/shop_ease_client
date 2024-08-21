3626a54bb19ebfae201513e8f8cc488611d3ce40
Naiem Hasan <naiemjoy1@gmail.com>
2024-08-21 10:49:15 +0600

    loaing delete

--- src/Pages/Home/Products/Products.jsx
+++ src/Pages/Home/Products/Products.jsx
@@ -99,121 +99,107 @@ const Products = () => {
 
   return (
     <div>
-      {loading ? (
-        <div className="text-center py-4">
-          <progress className="progress w-56"></progress>
-        </div> // Loading indicator
-      ) : (
-        <div>
-          <div className="lg:flex justify-between gap-8">
-            <div className="mb-4 flex flex-col gap-4 lg:w-1/4">
-              <input
-                type="text"
-                placeholder="Search for products"
-                value={searchQuery}
-                onChange={handleSearchChange}
-                className="p-2 border rounded w-full"
-              />
+      <div className="lg:flex justify-between gap-8">
+        <div className="mb-4 flex flex-col gap-4 lg:w-1/4">
+          <input
+            type="text"
+            placeholder="Search for products"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            className="p-2 border rounded w-full"
+          />
 
-              <div className="space-y-2 grid lg:grid-cols-1 grid-cols-2 items-center gap-2">
-                <select
-                  value={sortCriteria}
-                  onChange={handleSortChange}
-                  className="p-2 border rounded w-full"
-                >
-                  <option value="default">Sort By</option>
-                  <option value="price-low-high">Price: Low to High</option>
-                  <option value="price-high-low">Price: High to Low</option>
-                  <option value="date-newest">Date Added: Newest First</option>
-                </select>
+          <div className="space-y-2 grid lg:grid-cols-1 grid-cols-2 items-center gap-2">
+            <select
+              value={sortCriteria}
+              onChange={handleSortChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="default">Sort By</option>
+              <option value="price-low-high">Price: Low to High</option>
+              <option value="price-high-low">Price: High to Low</option>
+              <option value="date-newest">Date Added: Newest First</option>
+            </select>
 
-                <select
-                  value={brand}
-                  onChange={handleBrandChange}
-                  className="p-2 border rounded w-full"
-                >
-                  <option value="all">All Brands</option>
-                  {brands.map((brand) => (
-                    <option key={brand} value={brand}>
-                      {brand}
-                    </option>
-                  ))}
-                </select>
+            <select
+              value={brand}
+              onChange={handleBrandChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="all">All Brands</option>
+              {brands.map((brand) => (
+                <option key={brand} value={brand}>
+                  {brand}
+                </option>
+              ))}
+            </select>
 
-                <select
-                  value={category}
-                  onChange={handleCategoryChange}
-                  className="p-2 border rounded w-full"
-                >
-                  <option value="all">All Categories</option>
-                  {categories.map((category) => (
-                    <option key={category} value={category}>
-                      {category}
-                    </option>
-                  ))}
-                </select>
+            <select
+              value={category}
+              onChange={handleCategoryChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="all">All Categories</option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
 
-                <select
-                  value={priceRange}
-                  onChange={handlePriceRangeChange}
-                  className="p-2 border rounded w-full"
-                >
-                  <option value="all">All Price Ranges</option>
-                  <option value="low">Low (0 - 50)</option>
-                  <option value="mid">Mid (50 - 100)</option>
-                  <option value="high">High (100+)</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="lg:w-9/12">
-              <div className="grid lg:grid-cols-3 grid-cols-2 gap-5">
-                {products.map((product) => (
-                  <ProductsCard key={product._id} product={product} />
-                ))}
-              </div>
-
-              {/* Pagination */}
-              <div className="mt-8 flex justify-evenly items-center">
-                <button
-                  onClick={handlePrevPage}
-                  className="btn btn-sm btn-primary"
-                >
-                  Previous
-                </button>
-                <div>
-                  {pages.map((page) => (
-                    <button
-                      onClick={() => setCurrentPage(page)}
-                      key={page}
-                      className={`btn btn-sm ${
-                        page === currentPage ? "btn-active btn-primary" : ""
-                      }`}
-                    >
-                      {page + 1}
-                    </button>
-                  ))}
-                </div>
-                <button
-                  onClick={handleNextPage}
-                  className="btn btn-sm btn-primary"
-                >
-                  Next
-                </button>
-                <select
-                  value={itemsPerPage}
-                  onChange={handleItemsPerPage}
-                  className="ml-4"
-                >
-                  <option value="10">10</option>
-                  <option value="20">20</option>
-                  <option value="50">50</option>
-                </select>
-              </div>
-            </div>
+            <select
+              value={priceRange}
+              onChange={handlePriceRangeChange}
+              className="p-2 border rounded w-full"
+            >
+              <option value="all">All Price Ranges</option>
+              <option value="low">Low (0 - 50)</option>
+              <option value="mid">Mid (50 - 100)</option>
+              <option value="high">High (100+)</option>
+            </select>
           </div>
         </div>
-      )}
+
+        <div className="lg:w-9/12">
+          <div className="grid lg:grid-cols-3 grid-cols-2 gap-5">
+            {products.map((product) => (
+              <ProductsCard key={product._id} product={product} />
+            ))}
+          </div>
+
+          {/* Pagination */}
+          <div className="mt-8 flex justify-evenly items-center">
+            <button onClick={handlePrevPage} className="btn btn-sm btn-primary">
+              Previous
+            </button>
+            <div>
+              {pages.map((page) => (
+                <button
+                  onClick={() => setCurrentPage(page)}
+                  key={page}
+                  className={`btn btn-sm ${
+                    page === currentPage ? "btn-active btn-primary" : ""
+                  }`}
+                >
+                  {page + 1}
+                </button>
+              ))}
+            </div>
+            <button onClick={handleNextPage} className="btn btn-sm btn-primary">
+              Next
+            </button>
+            <select
+              value={itemsPerPage}
+              onChange={handleItemsPerPage}
+              className="ml-4"
+            >
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
